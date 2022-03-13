4712e0962a0ac4b57f5c4c23a35a4a4293927c00
VeronikaKorinchuk <veronikazen@list.ru>
2022-03-13 16:13:36 +0300

    Added driver capabilities getters

--- src/test/java/com/coherentsolutions/korinchuk/lab/java/web/utils/DriverFactory.java
+++ src/test/java/com/coherentsolutions/korinchuk/lab/java/web/utils/DriverFactory.java
@@ -1,10 +1,10 @@
 package com.coherentsolutions.korinchuk.lab.java.web.utils;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import java.util.Optional;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
@@ -24,6 +24,22 @@ public class DriverFactory {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("start-maximized"));
         return driver;
+    }
+
+    public static Capabilities getDriverCapabilities() {
+        return ((RemoteWebDriver) drivers.get()).getCapabilities();
+    }
+
+    public static String getBrowserVersion(){
+        return (String)getDriverCapabilities().getCapability("browserVersion");
+    }
+
+    public static String getBrowserName(){
+        return getDriverCapabilities().getBrowserName();
+    }
+
+    public static String getPlatform() {
+        return (String)getDriverCapabilities().getCapability("platform");
     }
 
     public void removeDriver() {
